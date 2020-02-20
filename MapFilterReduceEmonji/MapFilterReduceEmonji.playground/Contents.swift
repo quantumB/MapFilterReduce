77556b50aec0110f1317d13da7b8e49badf5d797
Bernardino Guerrero Ibarra <bernardino.guerrero@gmail.com>
2020-02-20 00:42:42 -0600

    Map, Filter and Reduce with emonji

--- MapFilterReduceEmonji/MapFilterReduceEmonji.playground/Contents.swift
+++ MapFilterReduceEmonji/MapFilterReduceEmonji.playground/Contents.swift
@@ -1,3 +1,53 @@
 import UIKit
 
-var str = "Hello, playground"
+let 🐮 = "🐮"
+let 🥔 = "🥔"
+let 🐔 = "🐔"
+let 🌽 = "🌽"
+let 🍔 = "🍔"
+let 🍟 = "🍟"
+let 🍗 = "🍗"
+let 🍿 = "🍿"
+let 💩 = "💩"
+
+func cook(_ rawFood: String) -> String {
+    switch rawFood {
+    case 🌽:
+        return 🍿
+    case 🐔:
+        return 🍗
+    case 🐮:
+        return 🍔
+    case 🥔:
+        return 🍟
+    default:
+        return ""
+    }
+    
+}
+
+func isVegeatrian(_ rawFood: String) -> Bool {
+    switch rawFood {
+    case  🍟, 🍿 :
+        return true
+    case 🍗, 🐮:
+        return false
+    default:
+        return false
+    }
+}
+
+func eat(_ food: String) -> String {
+    return 💩
+}
+
+
+let food = [🐮, 🥔, 🐔, 🌽].map{ cook($0) }
+let vegetarianFood = food.filter{ isVegeatrian($0)}
+let reducedTo = food.reduce("") { (_, food) -> String in
+    return eat(food)
+}
+
+print("food..:\(food)")
+print("vegetarianFood..: \(vegetarianFood)")
+print("reducedTo..:\(reducedTo)")
